0fe5ea3ec2e1c3d8e30e29d1b04c48618a1ee53d
mynSoo Wu <>
2018-05-19 19:40:11 +0800

    1

---
+++ FM/FM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FM
+//
+//  Created by mynSoo Wu on 2018/5/19.
+//  Copyright © 2018年 mynSoo Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FM/FM/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FM
+//
+//  Created by mynSoo Wu on 2018/5/19.
+//  Copyright © 2018年 mynSoo Wu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
